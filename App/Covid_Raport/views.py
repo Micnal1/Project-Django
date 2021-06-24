2a655693361a53dddf7f6e15cbbeb54a5723c3f6
Michał Zając <diuf95o@gmail.com>
2021-06-24 15:23:52 +0200

    add search

--- App/Covid_Raport/views.py
+++ App/Covid_Raport/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .api_client import get_raport, search_country
 from datetime import date
+from .forms import SearchForm
 
 
 # Create your views here.
@@ -10,8 +11,14 @@ def index(request):
 
 def Home_covid(request):
     country_list = search_country()
-
-    data = {'data': country_list}
+    form = SearchForm
+    data = {'data': country_list, 'form':form}
+    if request.method == "POST":
+        if form.is_valid:
+            form_data = form(request.POST).data.dict().pop('query')
+            print(form_data)
+            data = {'data':search_country(form_data), 'form':form}
+            return render(request, 'result.html', data)
 
     return render(request, 'home.html', data)
 
@@ -19,7 +26,11 @@ def Home_covid(request):
 def Country_detalis(request,country):
     time = date.today()
     raport = get_raport(country, time)
+    form = SearchForm
 
-    data = {'data': raport, 'number': raport}
+    data = {'data': raport, 'number': raport,'form':form}
 
     return render(request, 'country_detalis.html', data)
+
+
+
